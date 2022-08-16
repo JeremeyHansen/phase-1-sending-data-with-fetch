592ecd842145a7430145a24bb91f4cf4f711f3e3
Jeremey Hansen <jeremeyhansen@yahoo.com>
2022-08-15 22:22:08 -0400

    commit tree d51b6e86236e10e81744316b1df0757f92b527ca

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Add your code here
+// // Add your code here
+// const practiceObj = {
+//     name: "Jeremey",
+//     email: "jeremeyhansen@yahoo.com"
+// };
+
+// const submitData = {
+//     method: "POST",
+//     headers: {
+//         "Content-Type": "application/json",
+//         "Accept": "application/json",
+//     },
+//     body: JSON.stringify(practiceObj),
+// }
+
+// fetch("http://localhost:3000/users", submitData)
+// .then(res => res.json())
+// .then(data => console.log(data))
+// .catch(error => alert("uh oh"), console.log(error))
+    // this.name = name;
+    // this.email = email;
+
+
+
+
+
+
+function submitData(name, email) {
+    const ourRequest = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({name: `${name}`, email: `${email}`}),
+    }
+    return fetch("http://localhost:3000/users", ourRequest)
+    .then(res => res.json())
+    .then(object => document.body.innerHTML = object["id"]) 
+    .catch(err => document.body.innerHTML = err["message"]);
+}
